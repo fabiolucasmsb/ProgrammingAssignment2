95ac5ad455b1db2b6e25b335a9833ba3861e2d2e
Fábio Lucas Meira de Souza Barbosa <fabiolucasmsb@gmail.com>
2015-05-23 11:46:17 -0300

    Problem resolved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions calculate de inversed of a non square matrix
 
-## Write a short comment describing this function
+## THis function create a list that manipulate a matrix using gets and sets of the data and 
+## the inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inversedMatrix <- NULL
+        set <- function(y)
+        {
+                x <<- y
+                inversedMatrix <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inversedMatrix <<- inverse
+        getInverse <- function() inversedMatrix
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function try to get the inversed matrix at the cached data and if doesn't exist then calculate
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        library(MASS)
+        inversedMatrix <- x$getInverse()
+        if(!is.null(inversedMatrix)) {
+                return(inversedMatrix)
+        }
+        data <- x$get()
+        inversedMatrix <- ginv(matX)
+        x$setInverse(inversedMatrix)
+        inversedMatrix
 }
+
+
